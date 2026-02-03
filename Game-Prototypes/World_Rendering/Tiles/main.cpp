5735186fe0571bd286311544c90b3ef63c153a00
Londa Mngadi <mngadilg@gmail.com>
2026-02-03 21:01:31 +0200

    working on tile map generation, I beleive I've successfully made a mapping method, still need to test and refactor it,need to make the map a made outside the Mapp functiona and passed into it

--- Game-Prototypes/World_Rendering/Tiles/main.cpp
+++ Game-Prototypes/World_Rendering/Tiles/main.cpp
@@ -1,22 +1,100 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <fstream>
+#include <iostream>
 
-class Tile()
+std::vector<sf::Sprite> sMapping(std::string fileName)
 {
-    float x;
-    float y;
-    float width;
-    float height;
-};
+    sf::Sprite sprite;
+    sf::Texture texture;
+    std::string line,value;
+    int width = 64,height = 64;
+    std::ifstream file(fileName);
+    std::vector<sf::Sprite> tiles;
+    int column = 0,row = 0,start = 0;
+    std::map<std::string,sf::IntRect> map;
 
-void sLoadMap()
-{
+    // write out map
+    map["0"] = sf::IntRect(0,0,width,height);
+    map["1"] = sf::IntRect(64,0,width,height);
+    map["2"] = sf::IntRect(128,0,width,height);
+    map["3"] = sf::IntRect(192,0,width,height);
+    map["5"] = sf::IntRect(320,0,width,height);
+    map["6"] = sf::IntRect(384,0,width,height);
+    map["7"] = sf::IntRect(448,0,width,height);
+    map["8"] = sf::IntRect(512,0,width,height);
+    map["9"] = sf::IntRect(0,64,width,height);
+    map["10"] = sf::IntRect(64,64,width,height);
+    map["11"] = sf::IntRect(128,64,width,height);
+    map["12"] = sf::IntRect(193,64,width,height);
+    map["14"] = sf::IntRect(320,64,width,height);
+    map["15"] = sf::IntRect(384,64,width,height);
+    map["16"] = sf::IntRect(448,64,width,height);
+    map["17"] = sf::IntRect(512,64,width,height);
+    map["18"] = sf::IntRect(0,128,width,height);
+    map["19"] = sf::IntRect(64,128,width,height);
+    map["20"] = sf::IntRect(128,128,width,height);
+    map["21"] = sf::IntRect(192,128,width,height);
+    map["23"] = sf::IntRect(320,128,width,height);
+    map["24"] = sf::IntRect(384,128,width,height);
+    map["25"] = sf::IntRect(448,128,width,height);
+    map["26"] = sf::IntRect(512,128,width,height);
+    map["27"] = sf::IntRect(0,192,width,height);
+    map["28"] = sf::IntRect(64,192,width,height);
+    map["29"] = sf::IntRect(128,192,width,height);
+    map["30"] = sf::IntRect(192,192,width,height);
+    map["32"] = sf::IntRect(320,192,width,height);
+    map["33"] = sf::IntRect(384,192,width,height);
+    map["34"] = sf::IntRect(448,192,width,height);
+    map["35"] = sf::IntRect(512,192,width,height);
+    map["36"] = sf::IntRect(0,256,width,height);
+    map["39"] = sf::IntRect(192,256,width,height);
+    map["41"] = sf::IntRect(320,256,width,height);
+    map["42"] = sf::IntRect(384,256,width,height);
+    map["43"] = sf::IntRect(448,256,width,height);
+    map["44"] = sf::IntRect(512,256,width,height);
+    map["45"] = sf::IntRect(0,320,width,height);
+    map["48"] = sf::IntRect(192,320,width,height);
+    map["50"] = sf::IntRect(320,320,width,height);
+    map["51"] = sf::IntRect(384,320,width,height);
+    map["52"] = sf::IntRect(448,320,width,height);
+    map["53"] = sf::IntRect(512,320,width,height);
 
+    texture.loadFromFile("tiles/Tilemap_color1.png");
+    while(file >> line)
+    {
+        for(int i=0; i<line.length();i++)
+        {
+            if(line[i] == ',')
+            {
+                for(int j=(start + 1); j<i; j++)
+                {
+                    value += line[j];
+                }
+                // Generate sprite from value
+                sprite.setTexture(texture);
+                sprite.setTextureRect(map[value]);
+                sprite.setPosition((width*column),(height*row));
+                tiles.push_back(sprite);
+                
+                start = i;
+                value = "";
+            }
+
+            ++column;
+        }
+        ++row;
+    }
+
+    return tiles;
 }
  
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800,600), "Window");
     window.setFramerateLimit(60);
+
+    sMapping("csv/topDownMap_Island.csv");
 
     // Game loop
     while (window.isOpen())
