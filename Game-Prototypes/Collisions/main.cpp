b4c707504d555d2eb42357bf501e5ada335e688d
Londa Mngadi <mngadilg@gmail.com>
2026-02-21 20:01:57 +0200

    Converted to an Entity based system,still working on collision handling

--- Game-Prototypes/Collisions/main.cpp
+++ Game-Prototypes/Collisions/main.cpp
@@ -1,132 +1,254 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
+#include <memory>
+#include <vector>
+#include <string>
+#include <tuple>
+#include <map>
 
-class cBoundingBox
+class cShape;
+class GameEngine;
+class cTransform;
+class cBoundingBox;
+
+using ComponentTuple = std::tuple<cTransform,cBoundingBox,cShape>;
+
+class Component
+{
+    public:
+        bool exists = false;
+};
+
+class cShape: public Component
+{
+    public:
+        sf::RectangleShape rect;
+
+    cShape(){}
+
+    cShape(sf::Vector2f size,sf::Vector2f position,sf::Color color)
+    {
+        rect.setSize(size);
+        rect.setFillColor(color);
+        rect.setPosition(position);
+        rect.setOutlineThickness(2);
+        rect.setOutlineColor(color);
+    }
+    void updatePosition(sf::Vector2f position)
+    {
+        rect.setPosition(position);
+    }
+};
+
+class cTransform: public Component
+{
+    public:
+        sf::Vector2f velocity;
+        sf::Vector2f maxVelocity;
+        sf::Vector2f acceleration;
+        sf::Vector2f currentPosition;
+        sf::Vector2f previousPosition;
+
+    cTransform(){}
+
+    cTransform(sf::Vector2f pCPosition,sf::Vector2f pVelocity,sf::Vector2f pAcceleration,sf::Vector2f pMaxVelocity)
+    {
+        velocity = pVelocity;
+        maxVelocity = pMaxVelocity;
+        acceleration = pAcceleration;
+        currentPosition = pCPosition;
+    }
+
+    void updatePosition(sf::Vector2f pCPosition)
+    {
+        previousPosition = currentPosition;
+        currentPosition = pCPosition;
+    }
+};
+
+class cBoundingBox: public Component
 {
     public:
         float top;
         float left;
         float right;
         float bottom;
-        sf::Vector2f center;
         sf::FloatRect rect;
 
-        cBoundingBox(){}
+    cBoundingBox(){}
 
-        cBoundingBox(sf::FloatRect pRect)
-        {
-            updateBox(pRect);
-        }
-
-        void updateBox(sf::FloatRect pRect)
-        {
-            rect = pRect;
-            top = rect.top;
-            left = rect.left;
-            right = rect.left + rect.width;
-            bottom = rect.top + rect.height;
-
-            center.x = right/2;
-            center.y = bottom/2;
-        }
-        void updatePosition(sf::Vector2f pos)
-        {
-            top = pos.y;
-            left = pos.x;
-            right = pos.x + rect.width;
-            bottom = pos.y + rect.height;
-        }
-        sf::Vector2f getPosition()
-        {
-            return sf::Vector2f(left,top);
-        }
+    cBoundingBox(sf::FloatRect pRect)
+    {
+        updateBox(pRect);
+    }
+    void updateBox(sf::FloatRect pRect)
+    {
+        rect = pRect;
+        top = rect.top;
+        left = rect.left;
+        right = rect.left + rect.width;
+        bottom = rect.top + rect.height;
+    }
+    void updatePosition(sf::Vector2f pos)
+    {
+        top = pos.y;
+        left = pos.x;
+        right = pos.x + rect.width;
+        bottom = pos.y + rect.height;
+    }
+    sf::Vector2f getPosition()
+    {
+        return sf::Vector2f(left,top);
+    }
 
 };
 
-bool CollistionDetection(cBoundingBox b1,cBoundingBox b2)
+class Entity
 {
-    if(b1.right < b2.left)
-    {
-        return false;
-    }
-    if(b1.left > b2.right)
-    {
-        return false;
-    }
-    if(b1.bottom < b2.top)
-    {
-        return false;
-    }
-    if(b1.top > b2.bottom)
-    {
-        return false;
-    }
+    private:
+        int id;
+        bool alive;
+        std::string tag;
+        ComponentTuple components;
 
-    return true;
-}
+    public:
+        Entity(bool pAlive,std::string pTag,int pID)
+        {
+            id = pID;
+            tag = pTag;
+            alive = pAlive;
+        }
 
-struct Entity
-{
-    cBoundingBox bBox;
-    sf::RectangleShape rect;
+        void destroy()
+        {
+            alive = false;
+        }
+        
+        template<typename T,typename... TArgs>
+        T& addComponent(TArgs&&... mArgs)
+        {
+            auto & component = std::get<T>(components);
+            component = T(std::forward<TArgs>(mArgs)...);
+            component.exists = true;
+
+            return component;
+        }
+
+        template<typename T>
+        bool hasComponent()
+        {
+            return std::get<T>().exists;
+        }
+
+        template<typename T>
+        T& getComponent()
+        {
+            return std::get<T>(components);
+        }
+
+        template<typename T>
+        void removeComponent()
+        {
+            std::get<T>().exist = false;
+        }
 };
 
-void CollistionHandling(Entity& light,Entity heavy)
+
+class Physics
 {
-    // Horizontal
-    if(light.bBox.left < heavy.bBox.right)
-    {
-        light.bBox.left = heavy.bBox.right;
-        light.rect.setPosition(light.bBox.getPosition());
-    }
-    if(light.bBox.right > heavy.bBox.left)
-    {
-        light.bBox.right = heavy.bBox.left;
-        light.rect.setPosition(light.bBox.getPosition());
-    }
-    // vertical
-    if(light.bBox.top < heavy.bBox.bottom)
-    {
-        light.bBox.top = heavy.bBox.bottom;
-        light.rect.setPosition(light.bBox.getPosition());
-    }
-    if(light.bBox.bottom > heavy.bBox.top)
-    {
-        light.bBox.bottom = heavy.bBox.top;
-        light.rect.setPosition(light.bBox.getPosition());
-    }
+    public:
+        Physics(){};
+
+        bool collisionDetection(std::shared_ptr<Entity> ent1,std::shared_ptr<Entity> ent2)
+        {
+            cBoundingBox b1 = ent1->getComponent<cBoundingBox>();
+            cBoundingBox b2 = ent2->getComponent<cBoundingBox>();
+
+            if(b1.right < b2.left)
+            {
+                return false;
+            }
+            if(b1.left > b2.right)
+            {
+                return false;
+            }
+            if(b1.bottom < b2.top)
+            {
+                return false;
+            }
+            if(b1.top > b2.bottom)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        void collisionHandling(std::shared_ptr<Entity> ent1,std::shared_ptr<Entity> ent2)
+        {
+        }
+};
+
+void sMovement(std::shared_ptr<Entity> ent1,sf::Vector2f vel)
+{
+    sf::Vector2f accel = ent1->getComponent<cTransform>().currentPosition + vel;
+    ent1->getComponent<cTransform>().updatePosition(accel);
+    sf::Vector2f pos = ent1->getComponent<cTransform>().currentPosition;
+    ent1->getComponent<cBoundingBox>().updatePosition(pos);
+    ent1->getComponent<cShape>().updatePosition(pos);
 }
 
- 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800,600), "Window");
+    sf::RenderWindow window(sf::VideoMode(800,600),"Collisions");
     window.setFramerateLimit(60);
 
-    sf::FloatRect r1 = sf::FloatRect(0,0,50,50);
+    // Transform
+    sf::Vector2f velocity,maxVelocity;
+    sf::Vector2f position1,position2,position3,position4;
+    sf::Vector2f acceleration1,acceleration2,acceleration3,acceleration4;
+    position1 = sf::Vector2f(0,0);
+    position2 = sf::Vector2f(750,0);
+    position3 = sf::Vector2f(400,0);
+    position4 = sf::Vector2f(400,550);
+    velocity = sf::Vector2f(0,0);
+    maxVelocity = sf::Vector2f(0,0);
+    acceleration1 = sf::Vector2f(1.5,0);
+    acceleration2 = sf::Vector2f(-1.5,0);
+    acceleration3 = sf::Vector2f(0,1.5);
+    acceleration4 = sf::Vector2f(0,-1.5);
 
-    sf::Vector2f pos1,pos2,size;
-    pos1.x = 0;
-    pos1.y = 0;
+    // Shape
+    sf::Vector2f size = sf::Vector2f(50,50);
+    sf::Color color3 = sf::Color::Red;
+    sf::Color color1 = sf::Color::Black;
+    sf::Color color2 = sf::Color::White;
+    sf::Color color4 = sf::Color::Green;
 
-    pos2.y = 0;;
-    pos2.x = 750;
+    // Entities
+    std::shared_ptr<Entity> ent3 = std::make_shared<Entity>(true,"vertical1",2);
+    std::shared_ptr<Entity> ent4 = std::make_shared<Entity>(true,"vertical2",3);
+    std::shared_ptr<Entity> ent1 = std::make_shared<Entity>(true,"horizontal1",0);
+    std::shared_ptr<Entity> ent2 = std::make_shared<Entity>(true,"horizontal2",1);
 
-    Entity e1,e2;
+    //phy.collisionHandling(ent1,ent2);
 
-    e1.rect.setPosition(pos1);
-    e1.rect.setSize(sf::Vector2f(50,50));
-    e1.rect.setFillColor(sf::Color::Blue);
-    e1.bBox.updateBox(r1);
+    ent1->addComponent<cShape>(size,position1,color1);
+    ent2->addComponent<cShape>(size,position2,color2);
+    ent3->addComponent<cShape>(size,position3,color3);
+    ent4->addComponent<cShape>(size,position4,color4);
+    ent1->addComponent<cBoundingBox>(sf::FloatRect(position1,size));
+    ent2->addComponent<cBoundingBox>(sf::FloatRect(position2,size));
+    ent3->addComponent<cBoundingBox>(sf::FloatRect(position3,size));
+    ent4->addComponent<cBoundingBox>(sf::FloatRect(position4,size));
+    ent1->addComponent<cTransform>(position1,velocity,acceleration1,maxVelocity);
+    ent2->addComponent<cTransform>(position2,velocity,acceleration1,maxVelocity);
+    ent3->addComponent<cTransform>(position3,velocity,acceleration2,maxVelocity);
+    ent4->addComponent<cTransform>(position4,velocity,acceleration2,maxVelocity);
 
-    e2.rect.setPosition(pos2);
-    e2.rect.setSize(sf::Vector2f(50,50));
-    e2.rect.setFillColor(sf::Color::White);
-    e2.bBox.updateBox(r1);
-
-    // Game loop
-    while (window.isOpen())
+    while(window.isOpen())
     {
         sf::Event event;
         while(window.pollEvent(event))
@@ -136,28 +258,42 @@ int main()
                 window.close();
             }
         }
-        
-        pos1.x += 0.5;
-        pos2.x -= 0.5;
-        e1.rect.setPosition(pos1);
-        e1.bBox.updatePosition(pos1);
-        e2.rect.setPosition(pos2);
-        e2.bBox.updatePosition(pos2);
 
-        if(CollistionDetection(e1.bBox,e2.bBox))
+        // Update
+        sMovement(ent1,acceleration1);
+        sMovement(ent2,acceleration2);
+        sMovement(ent3,acceleration3);
+        sMovement(ent4,acceleration4);
+
+        // Physics
+        Physics phy = Physics();
+        if(phy.collisionDetection(ent1,ent2))
         {
-            CollistionHandling(e1,e2);
-            std::cout << "Collision" << std::endl;
+            std::cout << "Horizontal collision: Yes" << std::endl;
         }
-        else{
-            std::cout << "No Collision" << std::endl;
+        else
+        {
+            std::cout << "Horizontal collision: No" << std::endl;
         }
-
+/*
+        if(phy.collisionDetection(ent3,ent4))
+        {
+            std::cout << "Vertical collision: Yes" << std::endl;
+        }
+        else
+        {
+            std::cout << "Vertical collision: No" << std::endl;
+        }
+*/
+        // Render
         window.clear(sf::Color::Yellow);
-        window.draw(e1.rect);
-        window.draw(e2.rect);
+        window.draw(ent1->getComponent<cShape>().rect);
+        window.draw(ent2->getComponent<cShape>().rect);
+        //window.draw(ent3->getComponent<cShape>().rect);
+        //window.draw(ent4->getComponent<cShape>().rect);
         window.display();
     }
 
     return EXIT_SUCCESS;
 }
+
