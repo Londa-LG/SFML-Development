72c437cfe9ae61a773fd5bae90b68516104d3d63
Londa Mngadi <mngadilg@gmail.com>
2026-06-02 15:00:37 +0200

    Changed file structure, add assets and wrote Inventory.h file

---
+++ Game-Prototypes/Inventory_System/include/Inventory.h
@@ -0,0 +1,82 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <map>
+
+enum BodyPart{
+    feet,
+    legs,
+    head,
+    torso,
+    hands
+};
+
+struct Money{
+    int value;
+    int amount;
+    std::string name;
+    sf::Sprite sprite;
+};
+
+struct Weapon{
+    int id;
+    int damage;
+    int durability;
+    int trade_value;
+    bool one_hander;
+    int required_strenght;
+
+    std::string name;
+    sf::Sprite sprite;
+};
+struct Armour{
+    int id;
+    int protection;
+    int durability;
+    int trade_value;
+    BodyPart body_part;
+    int required_strenght;
+
+    std::string name;
+    sf::Sprite sprite;
+};
+struct QuestItem{
+    int id;
+    int quest_id;
+
+    std::string name;
+    sf::Sprite sprite;
+};
+struct Consumable{
+    int id;
+    int health;
+    int stamina;
+    int agility;
+    int trade_value;
+
+    std::string name;
+    sf::Sprite sprite;
+};
+
+struct Inventory{
+    int space;
+    Money gold;
+    std::map<int,Weapon> weapons;
+    std::map<int,Armour> armour;
+    std::map<int,QuestItem> quest_items;
+    std::map<int,Consumable> potions;
+    std::map<int,Consumable> food;
+};
+
+void collect_item_money(Inventory& inv,int amount);
+void collect_item_food(Inventory& inv,Consumable food);
+void collect_item_weapon(Inventory& inv,Weapon& weapon);
+void collect_item_armour(Inventory& inv,Armour& armour);
+void collect_item_potion(Inventory& inv,Consumable potion);
+void collect_item_quest_item(Inventory& inv,QuestItem q_item);
+
+void drop_item_food(Iventory& inv,int food_id);
+void drop_item_money(Inventory& inv,int amount);
+void drop_item_potion(Inventory& inv,int potion_id);
+void drop_item_weapon(Inventory& inv,int weapon_id);
+void drop_item_armour(Inventory& inv,int armout_id);
+void drop_item_quest_item(Inventory& inv,int questy_item_id);
---
+++ Game-Prototypes/Inventory_System/src/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include "Inventory.h"
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800,600), "Window");
+    window.setFramerateLimit(60);
+
+    // Game loop
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Yellow);
+        window.draw(w1.sprite);
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
